527519de342ed0fa69ea65bc60b2865d69b79b4b
jesusborrome <jesus.diaz@alunos.sc.senac.br>
2025-08-28 14:36:13 -0300

    som

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [Header("SomDoCorvo")]
+    public float somDoCorvo;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
