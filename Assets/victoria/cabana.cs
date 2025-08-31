d4bc2c904fcf5c72cbe986b370c60962d9a3ba9b
victoriaLM230 <victoria.martins1@alunos.sc.senac.br>
2025-08-31 18:28:35 -0300

    cenarios

---
+++ Assets/victoria/cabana.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cabana : MonoBehaviour
+{
+    [SerializeField] private string gameSceneName = "GameScene";
+
+    public void coiso()
+    {
+        print("ooooo");
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
