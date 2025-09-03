25069afb7b24af8adc679ec8ea7abe2653f77205
jesusborrome <jesus.diaz@alunos.sc.senac.br>
2025-09-03 14:22:13 -0300

    inplemantaçao de som

--- Assets/Scripts/MoveSabrina.cs
+++ Assets/Scripts/MoveSabrina.cs
@@ -1,58 +1,55 @@
 using UnityEngine;
 
-
 public class MoveSabrina : MonoBehaviour
 {
-
-
     [Header("Movimento")]
-
     public float velocidade = 5f;
-
     public Transform checagemChao;
-
     public float raioChao = 0.2f;
-
     public LayerMask camadaChao;
-
 
     private float horizontal;
     private Rigidbody2D rb;
-
     private bool estaNoChao;
-    public bool PodeMexer;
+    public bool PodeMexer = true;
+
+    [Header("Som de Movimento")]
+    public AudioSource somMovimento;  // arraste o AudioSource no Inspector
 
     void Start()
-
     {
-
-
         rb = GetComponent<Rigidbody2D>();
-
-
     }
-
-
 
     void Update()
-
     {
-
-        // Verifica se est� no ch�o usando OverlapCircle 
-
+        // Verifica se está no chão usando OverlapCircle 
         estaNoChao = Physics2D.OverlapCircle(checagemChao.position, raioChao, camadaChao);
 
-
-
         // Movimento horizontal 
-
         horizontal = Input.GetAxisRaw("Horizontal");
 
+        if (PodeMexer)
+        {
+            // Substituído .velocity por .linearVelocity
+            rb.linearVelocity = new Vector2(horizontal * velocidade, rb.linearVelocity.y);
+        }
 
-        rb.linearVelocity = new Vector2(horizontal * velocidade, rb.linearVelocity.y);
-
-   
-
+        // Som de movimento (somente se pressionando A ou D)
+        if (estaNoChao && horizontal != 0)
+        {
+            if (!somMovimento.isPlaying)
+            {
+                somMovimento.Play();
+            }
+        }
+        else
+        {
+            if (somMovimento.isPlaying)
+            {
+                somMovimento.Stop();
+            }
+        }
+        print(horizontal);
     }
 }
-
