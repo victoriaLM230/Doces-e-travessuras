16396b3b78af90e335358b6c702960f40665102f
FelipeMacariMagalhaes <felipe.magalhaes@alunos.sc.senac.br>
2025-09-03 14:53:47 -0300

    Finalizamento do jogo

--- Assets/Scripts/GerenciadorDeDialogo.cs
+++ Assets/Scripts/GerenciadorDeDialogo.cs
@@ -16,18 +16,17 @@ public class GerenciadorDeDialogo : MonoBehaviour
     public string[] falasJogador;
 
     public GameObject npcBruxa; // Referência ao GameObject da bruxa
-    public BocaSeMechendo bocaAnimada; // ? Referência à boca animada
+ 
 
     private int indiceFala = 0;
     public bool dialogoAtivo = false;
     private bool esperandoEscolha = false;
 
-    public int moralidade = 0; // Começa neutro
+    
 
     void Start()
     {
-       // botaoBem.onClick.AddListener(EscolherBem);
-       // botaoMal.onClick.AddListener(EscolherMal);
+       
     }
 
     void Update()
@@ -47,7 +46,7 @@ public class GerenciadorDeDialogo : MonoBehaviour
         AtualizarFalas();
         dialogoAtivo = true;
 
-        Time.timeScale = 0f; // Pausa o jogo enquanto fala
+     
     }
 
     void AvancarFala()
@@ -76,51 +75,18 @@ public class GerenciadorDeDialogo : MonoBehaviour
         textoNPC.text = falasNPC[indiceFala];
         textoJogador.text = falasJogador[indiceFala];
 
-        if (bocaAnimada != null)
-            bocaAnimada.falando = true;
+       
     }
 
     void MostrarEscolhas()
     {
         painelEscolhas.SetActive(true);
     }
-
-    void EscolherBem()
-    {
-        moralidade += 1;
-        Debug.Log("Escolheu o bem. Moralidade: " + moralidade);
-        ChecarCaminho();
-    }
-
-    void EscolherMal()
-    {
-        moralidade -= 1;
-        Debug.Log("Escolheu o mal. Moralidade: " + moralidade);
-        ChecarCaminho();
-    }
-
-    void ChecarCaminho()
-    {
-        painelEscolhas.SetActive(false);
-        esperandoEscolha = false;
-
-        if (moralidade < 0)
-        {
-            textoNPC.text = "Você sente a escuridão crescendo...";
-        }
-        else
-        {
-            textoNPC.text = "A luz continua dentro de você.";
-        }
-
-        Invoke(nameof(FecharDialogo), 2f);
-    }
-
     public void FecharDialogo()
     {
         painelDialogo.SetActive(false);
         dialogoAtivo = false;
-        Time.timeScale = 1f;
+        
         if (npcBruxa != null)
         {
             npcBruxa.SetActive(false); // Ou: Destroy(npcBruxa);
---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    public void MudarParaCena()
+    {
+        SceneManager.LoadScene("Felipe");
+    }
+}
